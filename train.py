3e5ede3b9b44730a82aceadfe915723a7798cf15
keyEpoch <1801210747@pku.edu.cn>
2019-10-28 21:02:41 +0800

    add dcn configs for mobile0.25 and resnet50

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import torch.optim as optim
 import torch.backends.cudnn as cudnn
 import argparse
 import torch.utils.data as data
-from data import WiderFaceDetection, detection_collate, preproc, cfg_mnet, cfg_re50
+from data import WiderFaceDetection, detection_collate, preproc, cfg_mnet, cfg_re50, cfg_re50_sshdcn_v1, cfg_mnet_sshdcn_v1
 from layers.modules import MultiBoxLoss
 from layers.functions.prior_box import PriorBox
 import time
@@ -34,6 +34,10 @@ if args.network == "mobile0.25":
     cfg = cfg_mnet
 elif args.network == "resnet50":
     cfg = cfg_re50
+elif args.network == "mobile0.25_sshdcn":
+    cfg = cfg_mnet_sshdcn_v1
+elif args.network == "resnet50_sshdcn":
+    cfg = cfg_re50_sshdcn_v1
 
 rgb_mean = (104, 117, 123) # bgr order
 num_classes = 2
